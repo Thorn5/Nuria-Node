5ae86cc7c9552353418dc7a34d5cf8c02d6dfb9d
Nuria <c.odena.nuria@gmail.com>
2023-03-15 19:37:16 +0100

    first commit

---
+++ router/filmsRouter.js
@@ -0,0 +1,63 @@
+const { Pool } = require('pg')
+const express = require('express');
+const router = express.Router();
+
+const pool = new Pool();
+
+// GET time
+// router.get('/time', (req, res) => {
+//   pool
+//   .query('SELECT NOW()')
+//   .then(data => res.json(data.rows[0]))
+//   .catch(err => res.send(err))
+// })
+
+//GET Create an endpoint to retrieve all films
+router.get('/', (req, res) => {
+  pool
+  .query('SELECT * from films;')
+  .then(({rows}) => res.json(rows))
+  .catch(e => res.sendStatus(500))
+})
+
+//GET Create an endpoint that a specific film
+router.get("/:id", (req, res) => {
+  const id = req.params.id
+  pool
+  .query('SELECT * from films WHERE id=$1;', [id])
+  .then(({rows}) => res.json(rows))
+  .catch(e => res.sendStatus(500))
+});
+
+//POST Create an endpoint that creates a new film/row in films table
+router.post('/', (req, res) => {
+  const {name, year, genre } = req.body;
+  pool
+  .query('INSERT INTO films(name, year, genre ) VALUES ($1, $2, $3) RETURNING *;', [name, year, genre])
+  .then(({rows}) => res.json(rows))
+  .catch(e => res.sendStatus(500))
+})
+
+// PUT Create an enspoint that updates an existing film in films table
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const {name} = req.body;
+  pool
+  .query('UPDATE films SET name=$1 WHERE id=$2 RETURNING *;', [name, id])
+  .then(({rows}) => res.json(rows))
+  .catch(e => res.sendStatus(500))
+})
+
+//DELETE Create an endpoint that deletes an existing film in films table
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  pool
+  .query('DELETE FROM films WHERE id=$1;', [id])
+  .then(({rows}) => res.json(rows))
+  .catch(e => {
+    console.log(e, 'error')
+    res.sendStatus(500)
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,75 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8001
+const films = require('./router/filmsRouter');
+const bodyParser = require('body-parser');
 
-// GET method route
-    app.get('/', (req, res) => {
-        res.send('GET request to the homepage')
-    })
- 
-// POST method route
-  app.post('/', (req, res) => {
-    res.send('POST request to the homepage')
-  })
-  
-// PUT method route
-  app.put('/', (req, res) => {
-    res.send('PUT request to homepage')
-})
-  
-// DELETE method route
-  app.delete('/', (req, res) => {
-    res.send('DELETE request to homepage')
-})
-
-//CRUD METHODS THAT SHARE SAME ROUTE 
-// app
-// .route('/')
-// .get((req, res) => {
-//     res.send('GET request to the homepage')
-// })
-// .post((req, res) => {
-//     res.send('POST request to the homepage')
-//   })
-  
-// .put((req, res) => {
-//     res.send('PUT request to homepage')
-// })
-  
-// .delete((req, res) => {
-//     res.send('DELETE request to homepage')
-// })
-
-//LIST OF RECIPES
-
-const recipes = [
-    {id: 1, title: "Pizza"},
-    {id: 2, title: "Paella"}
-];
-
-//CREATE AN ENDPOINT TO RETRIEVE THE LIST OF RECIPES
-  app.get('/api/recipes', (req, res) => {
-    res.json(recipes);
-  })
-
-//CREATE AN ENDPOINT THAT RETRIEVES A SPECIF RECIPE BY ID
-
-  app.get('/api/recipes/:id', (req, res) => {
-    const id = req.params.id;
-    const recipe = recipes.find(recipe => recipe.id == id)  
-    if(!recipe){
-        return res.status(404).send('Recipe not found')
-    }
-    res.json(recipe)
-  })
-
-//Create an endpoint that redirects to another url
-
-app.get('/redirect', (req, res) => {
-    res.redirect('https://www.wbscodingschool.com/');
-});
-
-
+app.use(bodyParser.json())
+app.use('/api/films', films)
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
